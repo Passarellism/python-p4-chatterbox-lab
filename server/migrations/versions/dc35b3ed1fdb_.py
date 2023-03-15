4e47cad30ee2e752884ad7299701000f7813bbf6
steve passarelli <passarellism@gmail.com>
2023-03-15 09:50:24 -0600

    seed complete

---
+++ server/migrations/versions/dc35b3ed1fdb_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: dc35b3ed1fdb
+Revises: 0e12555d4c52
+Create Date: 2023-03-15 09:45:31.967055
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc35b3ed1fdb'
+down_revision = '0e12555d4c52'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
