de8b2bcc960c363cbb8680f1528a47978e47eec5
Bianca Aspin <bianca@mj94.net>
2023-03-15 09:16:46 -0700

    Build out initial POST route for messages, not functional yet.

--- server/app.py
+++ server/app.py
@@ -14,9 +14,29 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+        response = make_response(
+            messages,
+            200
+        )
+        return response
+    elif request.method == 'POST':
+        new_message = Message(
+            body = request.get("body"),
+            username = request.get("username")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        
+        response_body = new_message.to_dict()
+        response = make_response(
+            response_body, 
+            201
+        )
+        return response
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
